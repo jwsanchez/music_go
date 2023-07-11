0e5ff16e6cf7369abd96933115286b4a2ad1e9f3
Jesus W Sanchez <jwsanchez@outlook.es>
2023-07-10 21:05:50 -0500

    Redireccionando de home a intro

--- src/app/guards/intro.guard.ts
+++ src/app/guards/intro.guard.ts
@@ -2,14 +2,16 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Storage } from '@ionic/storage-angular';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IntroGuard implements CanActivate {
-  constructor(private storage: Storage){}
+  constructor(private storage: Storage, private router: Router){}
   canActivate(){
     console.log("No fue posible");
+    this.router.navigateByUrl("/intro");
     return false;
   }
   
