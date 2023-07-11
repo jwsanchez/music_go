56b4ab57bfd4d3165a986e7b66fe66b82032ee34
Jesus W Sanchez <jwsanchez@outlook.es>
2023-07-10 20:54:02 -0500

    Implementando Intro Guard

--- src/app/guards/intro.guard.ts
+++ src/app/guards/intro.guard.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { Storage } from '@ionic/storage-angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IntroGuard implements CanActivate {
+  constructor(private storage: Storage){}
   canActivate(){
-    return true;
+    console.log("No fue posible");
+    return false;
   }
   
 }
