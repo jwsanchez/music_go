b05b0ee4c81f16897bdea2fbd1f50b0990372f02
Jesus W Sanchez <jwsanchez@outlook.es>
2023-07-12 20:09:44 -0500

    Termiando validacion y config guard de login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { AuthenticateService } from '../services/authenticate.service';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -17,9 +19,13 @@ export class LoginPage implements OnInit {
       {type: "required", message: "Debe colocar una contraseña"},
       {type: "minlength", message: "Al menos debe contener 6 caracteres"}
     ]
-
   }
-  constructor(private formBuilder: FormBuilder) { 
+  errorMessage: string = '';
+  constructor(
+    private formBuilder: FormBuilder, 
+    private authService: AuthenticateService,
+    private navCtrl: NavController
+    ) { 
     this.loginForm = this.formBuilder.group(
       {
         email: new FormControl(
@@ -52,6 +58,13 @@ export class LoginPage implements OnInit {
 
   loginUser(credentials: any){
     console.log(credentials);
+    this.authService.loginUser(credentials).then(res => {
+      this.errorMessage = "";
+      this.navCtrl.navigateForward("/home");
+    }).catch(err => {
+      this.errorMessage = err;
+      alert(this.errorMessage)
+    })
   }
 
 }
