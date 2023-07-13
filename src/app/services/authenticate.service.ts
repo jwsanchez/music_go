8821296888752730bbec0dae2273254b6f534e21
Jesus W Sanchez <jwsanchez@outlook.es>
2023-07-12 19:34:13 -0500

    Validando credenciales

--- src/app/services/authenticate.service.ts
+++ src/app/services/authenticate.service.ts
@@ -8,6 +8,16 @@ export class AuthenticateService {
   constructor() { }
 
   loginUser(credentials: any){
-    
+    return new Promise((accept, reject) =>{
+      if(
+        credentials.email == "jesus@pca.edu.co" &&
+        credentials.password == "1234567"
+      )
+      {
+        accept("Login Exitoso")
+      }else{
+        reject("Verifique credenciales")
+      }
+     })
   }
 }
