e8e3d7d6314b382ca105619e5f02aec8bba42189
Jesus W Sanchez <jwsanchez@outlook.es>
2023-07-13 19:57:45 -0500

    Creando vista de formulario registro

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
-
-  constructor() { }
+  registerForm: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+    this.registerForm = this.formBuilder.group(
+      {
+        email: new FormControl(
+          "",
+          Validators.compose(
+            [
+              Validators.required,
+              Validators.pattern("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9_.+-]+.[a-zA-Z0-9.-]+$")
+            ]
+          )
+        ),
+        password: new FormControl(
+          "",
+          Validators.compose(
+            [
+              Validators.required,
+              Validators.minLength(6)
+            ]
+          )
+        ),
+        name: new FormControl(
+          "",
+          Validators.compose(
+            [
+              Validators.required,
+              Validators.minLength(6)
+            ]
+          )
+        ),
+        last_name: new FormControl(
+          "",
+          Validators.compose(
+            [
+              Validators.required,
+              Validators.minLength(6)
+            ]
+          )
+        )
+      }
+    )
+  }
 
   ngOnInit() {
   }
-
 }
