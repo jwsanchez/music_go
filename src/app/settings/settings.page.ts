ea67ccf9c9a217aa0f9c3eb74438093a25a9e345
Jesus W Sanchez <jwsanchez@outlook.es>
2023-07-25 20:45:13 -0500

    Update de foto haciendo post a la API

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -2,32 +2,42 @@ import { Component, OnInit } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { Camera, CameraResultType, CameraSource} from '@capacitor/camera';
 import { UserService } from '../services/user.service';
-
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.page.html',
   styleUrls: ['./settings.page.scss'],
 })
-export class SettingsPage implements OnInit {
+export class SettingsPage{
 userImage = "/assets/images/settingsprofile.png";
 photo: any;
-  constructor(private userService: UserService) { }
+userInfo: any;
+  constructor(private userService: UserService, private storage: Storage) { }
 
-  ngOnInit() {
-    this.userService.getUser(1).then(userData =>{
-      //TODO
-    });
+  ionViewDidEnter() {
+    this.userService.getUser().then(userData => {
+      this.userInfo = userData;
+      this.photo = userData.image;
+    }); 
   }
 
   async takePhoto(){
-    const image = await Camera.getPhoto({
-      quality: 100,
-      allowEditing: false,
-      resultType: CameraResultType.DataUrl,
-      source: CameraSource.Camera
-    });
-    this.photo = image.dataUrl;
-    console.log(image.dataUrl);
+    try{
+      const image = await Camera.getPhoto({
+        quality: 100,
+        allowEditing: false,
+        resultType: CameraResultType.DataUrl,
+        source: CameraSource.Camera
+      })
+      this.photo = image.dataUrl;
+      console.log(image.dataUrl);
+      this.userService.updateUser({"image": this.photo}).then((res: any) => {
+        console.log(res)
+      });
+    }catch(error){
+      console.log("No se selecciono foto");
+    }
+    
   }
 }
